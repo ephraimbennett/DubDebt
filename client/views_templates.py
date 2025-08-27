1c703a05515a0489a9c5ef6e4e6d24ebf01c391e
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-08-27 16:32:01 -0500

    got the loading of customs vs default working

--- client/views_templates.py
+++ client/views_templates.py
@@ -5,7 +5,7 @@ from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponseBadRequest
 
 from .models import Profile
-from main.models import MessageTemplate
+from main.models import MessageTemplate, CustomSMSTemplate, MessageTemplateRouter
 
 import json
 
@@ -22,7 +22,17 @@ def templates(request):
         'template': t.template
     } for t in templates]
 
+    # also grab the custom data if it exists
+    message_types = ["initial", "followup1", "followup2"]
+    sms_router, created = MessageTemplateRouter.objects.get_or_create(user=request.user.profile)
+    custom_data = []
+    for m in message_types:
+        item = sms_router.get_template(m)
+        custom_data.append({'title': item.title, 'template': item.template})
+
     context['templates'] = json.dumps(data)
+    print(custom_data)
+    context['custom_templates'] = json.dumps(custom_data)
 
     return render(request, "portal_templates.html", context)
 

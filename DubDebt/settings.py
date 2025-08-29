bcb34794e54b9c1f87e31df788f269aca3d4629d
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-08-28 22:22:27 -0500

    fixed secret thing

--- DubDebt/settings.py
+++ DubDebt/settings.py
@@ -179,11 +179,11 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Google OAuth Section
 GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
-if not GOOGLE_OAUTH_CLIENT_ID:
+'''if not GOOGLE_OAUTH_CLIENT_ID:
     raise ValueError(
         'GOOGLE_OAUTH_CLIENT_ID is missing.'
         'Have you put it in a file at core/.env ?'
-    )
+    )'''
  
 # We need these lines below to allow the Google sign in popup to work.
 SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
